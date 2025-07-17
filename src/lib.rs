bb1b7da67825726d8f2c1d3d8336292568ebc059
Ibuki.O <isan.ethen@gmail.com>
2025-07-17 15:00:06 +0900

    feat: Implemented attribute macros.

--- src/lib.rs
+++ src/lib.rs
@@ -4,8 +4,56 @@ use std::fs::OpenOptions;
 use std::io::Write;
 use syn::{
     parse::{Parse, ParseStream, Result},
-    parse_macro_input, ItemFn,
+    parse_macro_input, ItemFn, LitStr,
 };
+
+fn log_unimplemented_function(func_name: &str, comment: &str) {
+    let out_dir = match std::env::var("OUT_DIR") {
+        Ok(dir) => dir,
+        Err(_) => {
+            eprintln!("Warning: OUT_DIR not set. Cannot write to unimplemented_symbols.txt");
+            return;
+        }
+    };
+    let dest_path = std::path::Path::new(&out_dir).join("unimplemented_symbols.txt");
+
+    let mut file = OpenOptions::new()
+        .create(true)
+        .append(true)
+        .open(&dest_path)
+        .expect("Failed to open unimplemented_symbols.txt");
+
+    if comment.is_empty() {
+        writeln!(file, "{}", func_name).expect("Failed to write to file");
+    } else {
+        writeln!(file, "{} # {}", func_name, comment).expect("Failed to write to file");
+    }
+}
+
+#[proc_macro_attribute]
+pub fn unimplemented_function(attr: TokenStream, item: TokenStream) -> TokenStream {
+    let func = parse_macro_input!(item as ItemFn);
+
+    let comment = if !attr.is_empty() {
+        let lit_str = parse_macro_input!(attr as LitStr);
+        lit_str.value()
+    } else {
+        String::new()
+    };
+
+    let func_name = func.sig.ident.to_string();
+    log_unimplemented_function(&func_name, &comment);
+
+    let vis = &func.vis;
+    let sig = &func.sig;
+    let result = quote! {
+        #vis #sig {
+            unimplemented!()
+        }
+    };
+
+    result.into()
+}
 
 struct UnimplementedInput {
     functions: Vec<ItemFn>,
@@ -25,25 +73,15 @@ impl Parse for UnimplementedInput {
 pub fn unimplemented_functions(input: TokenStream) -> TokenStream {
     let UnimplementedInput { functions } = parse_macro_input!(input as UnimplementedInput);
 
-    let out_dir = std::env::var("OUT_DIR").expect("OUT_DIR must be set");
-    let dest_path = std::path::Path::new(&out_dir).join("unimplemented_symbols.txt");
-
-    let mut file = OpenOptions::new()
-        .create(true)
-        .append(true)
-        .open(&dest_path)
-        .expect("Failed to open unimplemented_symbols.txt");
-
     let generated_functions = functions.iter().map(|func| {
-        let sig = &func.sig;
+        let func_name = func.sig.ident.to_string();
+        log_unimplemented_function(&func_name, "");
+
         let vis = &func.vis;
-        let func_name = &sig.ident;
-
-        writeln!(file, "{}", func_name).expect("Failed to write to file");
-
+        let sig = &func.sig;
         quote! {
             #vis #sig {
-                unimplemented!();
+                unimplemented!()
             }
         }
     });
